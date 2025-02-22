d32d5a45083ce09eb87db8c9af9e5666a1e93c53
co-andrey-l <litvinov.andrey@outlook.com>
2025-02-22 21:15:45 +0300

    [features] Add issues command

--- main.py
+++ main.py
@@ -12,7 +12,12 @@ def start(update, context):  # pylint: disable=unused-argument
     """Handling the start function."""
     update.message.reply_text(
         "Привет! Отправь мне PINFL для анализа и я скажу валидный он или нет.\n"
-        "Тут все честно, я не сохраняю никаких данных о тебе или о том, что ты пишешь."
+        "Тут все честно, я не сохраняю никаких данных о тебе или о том, что ты пишешь.\n\n"
+        "Доступные команды:\n"
+        "/start - Начать работу с ботом\n"
+        "/generate - Сгенерировать случайный PINFL\n"
+        "/issues - Сообщить о проблеме\n"
+        "Или вы можете отправить мне PINFL для анализа обычным сообщением."
     )
 
 
@@ -28,12 +33,12 @@ def echo(update, context):  # pylint: disable=unused-argument
         else:
             response = "Неверный PINFL. Проверьте следующие ошибки:\n"
             if not parser.is_valid_date():
-                response += "- Неправильная дата.\n"
+                response += "- Неправильная дата рождения.\n"
             if not parser.validate_check_digit():
                 correct_check_digit = parser.calculate_check_digit()
                 response += (
                     "- Неправильная контрольная цифра. "
-                    f"Правильная контрольная цифра: {correct_check_digit}\n"
+                    f"Ожидаемая контрольная цифра: {correct_check_digit}\n"
                 )
             if not parser.validate_area_code():
                 response += "- Неправильный код региона.\n"
@@ -53,13 +58,21 @@ def generate_pinfl(update, context):  # pylint: disable=unused-argument
     generator = PinflUtilitiesGenerator()
     gender = random.choice(["male", "female"])
     birth_date = datetime.date(
-        random.randint(1900, 2005), random.randint(1, 12), random.randint(1, 28)
+        random.randint(1900, datetime.date.today().year - 19), random.randint(1, 12), random.randint(1, 28)
     )
     pinfl = generator.generate(gender, birth_date)
     formatted_birth_date = birth_date.strftime("%d %B\\(%m\\) %Y")
     update.message.reply_text(
-        f"```{pinfl}```\nДата рожденья: {formatted_birth_date}\nГендер: \\#{gender}",
+        f"```{pinfl}```\nДата рождения: {formatted_birth_date}\nПол: \\#{gender}",
         parse_mode="MarkdownV2",
+    )
+
+def issues(update, context):
+    """Handling the issue function."""
+    issue_link = os.environ.get("ISSUE_LINK")
+    update.message.reply_text(
+        "Если у вас возникли проблемы или у вас есть предложение, пожалуйста, "
+        f"оставьте [issue]({issue_link}) проекта."
     )
 
 
@@ -71,7 +84,8 @@ def main():
     dp = updater.dispatcher
 
     dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("generate_pinfl", generate_pinfl))
+    dp.add_handler(CommandHandler("generate", generate_pinfl))
+    dp.add_handler(CommandHandler("issues", issues))
     dp.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
 
     print("Bot launched successfully")
